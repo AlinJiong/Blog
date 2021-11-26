1dcf26c7f5bb3e1378fee13cd432f53033ee4721
AlinJiong <alinjiong@qq.com>
2021-11-26 21:46:36 +0800

    切换Windows环境

--- article/models.py
+++ article/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+from django.utils import timezone
+
+
+class ArticlePost(models.Model):
+    # 文章作者，制定数据的删除方式 作者1 文章 n
+    auther = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    create_time = models.DateTimeField(default=timezone.now)
+    update_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # 根据创建时间 倒序 排布
+        ordering = ('-create_time', )
+
+    def __str__(self):
+        return self.title
--- article/views.py
+++ article/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import ArticlePost
+
+
+def article_list(request):
+    articles = ArticlePost.objects.all()
+    return render(request, 'article/list.html', locals())
