36460e797eda3edd5300d67536bec5c85e03e52b
AlinJiong <alinjiong@qq.com>
2021-11-28 00:59:19 +0800

    markdown

--- article/views.py
+++ article/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from .models import ArticlePost
+import markdown
 
 
 def article_list(request):
@@ -13,4 +14,10 @@ def article_list(request):
 def article_detail(request, id):
 
     article = ArticlePost.objects.get(id=id)
+
+    article.content = markdown.markdown(article.content,
+                                        extensions=[
+                                            'markdown.extensions.extra',
+                                            'markdown.extensions.codehilite',
+                                        ])
     return render(request, 'article/detail.html', locals())
